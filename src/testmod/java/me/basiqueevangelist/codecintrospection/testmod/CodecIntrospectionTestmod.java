e4de08e12d96ad51cb2ed8a6bc6c27d598321acd
Basique <basiqueevangelist@yandex.ru>
2024-07-29 18:45:38 +0300

    make the testmod output a dump file and add all data components to testing

--- src/testmod/java/me/basiqueevangelist/codecintrospection/testmod/CodecIntrospectionTestmod.java
+++ src/testmod/java/me/basiqueevangelist/codecintrospection/testmod/CodecIntrospectionTestmod.java
@@ -2,15 +2,19 @@ package me.basiqueevangelist.codecintrospection.testmod;
 
 import com.mojang.serialization.Codec;
 import me.basiqueevangelist.codecintrospection.CodecIntrospection;
+import me.basiqueevangelist.codecintrospection.util.FormattedDumper;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 import net.minecraft.SharedConstants;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.Bootstrap;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeSpecialEffects;
-import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CodecIntrospectionTestmod implements PreLaunchEntrypoint {
     @Override
@@ -18,17 +22,30 @@ public class CodecIntrospectionTestmod implements PreLaunchEntrypoint {
         SharedConstants.tryDetectVersion();
         Bootstrap.bootStrap();
 
-        demo("Codec.STRING", Codec.STRING);
-        demo("ResourceLocation.CODEC", ResourceLocation.CODEC);
-        demo("ItemStack.CODEC", ItemStack.CODEC);
-        demo("Biome.DIRECT_CODEC", Biome.DIRECT_CODEC);
-        demo("BiomeSpecialEffects.CODEC", BiomeSpecialEffects.CODEC);
-        demo("ItemEnchantments.CODEC", ItemEnchantments.CODEC);
+        try (var os = Files.newOutputStream(Path.of("codec-introspection-testmod-dump.txt"))) {
+            FormattedDumper dumper = new FormattedDumper(new PrintStream(os));
+
+            demo(dumper, "Codec.STRING", Codec.STRING);
+            demo(dumper, "ResourceLocation.CODEC", ResourceLocation.CODEC);
+            demo(dumper, "ItemStack.CODEC", ItemStack.CODEC);
+            demo(dumper, "Biome.DIRECT_CODEC", Biome.DIRECT_CODEC);
+
+            for (var componentType : BuiltInRegistries.DATA_COMPONENT_TYPE) {
+                var codec = componentType.codec();
+
+                if (codec == null) continue;
+
+                demo(dumper, BuiltInRegistries.DATA_COMPONENT_TYPE.getKey(componentType).toString(), codec);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         System.exit(0);
     }
 
-    private void demo(String name, Codec<?> codec) {
-        LoggerFactory.getLogger("CodecIntrospectionTestmod").info("{} => \n{}", name, CodecIntrospection.introspect(codec).dumpToString());
+    private void demo(FormattedDumper dumper, String name, Codec<?> codec) {
+        dumper.write(0, name + " => ");
+        CodecIntrospection.introspect(codec).dump(dumper, 1);
     }
 }
